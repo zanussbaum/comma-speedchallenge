40b92507486285e4af40ba264209df9165610a46
zanussbaum <zanussbaum@gmail.com>
2020-07-29 14:56:25 -0400

    data extraction and augmentation

--- data_scripts/img_to_flow.py
+++ data_scripts/img_to_flow.py
@@ -1,6 +1,7 @@
 import cv2
 import pickle
 import numpy as np
+import sys
 
 
 table = np.empty((1,256), np.uint8)
@@ -21,50 +22,91 @@ def canny_edge_detector(image):
     canny = cv2.Canny(blur, 50, 150) 
     return canny
 
-cap = cv2.VideoCapture("../video/train.mp4")
-ret, frame1 = cap.read()
-prvs = canny_edge_detector(frame1)
-hsv = np.zeros_like(frame1)
-hsv[...,1] = 255
 
-with open("../video/train.txt", 'r') as f:
-    speeds = f.read().split('\n')
+def img_to_flow_train():
+    cap = cv2.VideoCapture("../video/train.mp4")
+    ret, frame1 = cap.read()
+    prvs = canny_edge_detector(frame1)
+    hsv = np.zeros_like(frame1)
+    hsv[...,1] = 255
 
-speeds = [float(s) for s in speeds]
-mean_speeds = []
+    with open("../video/train.txt", 'r') as f:
+        speeds = f.read().split('\n')
 
-num_frames = 0
+    speeds = [float(s) for s in speeds]
 
-while(1):
-    ret, frame2 = cap.read()
-    next = canny_edge_detector(frame2)
+    num_frames = 0
 
-    flow = cv2.calcOpticalFlowFarneback(prvs,next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+    while(1):
+        ret, frame2 = cap.read()
+        next = canny_edge_detector(frame2)
 
-    mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
-    hsv[...,0] = ang*180/np.pi/2
-    hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
-    rgb = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
-    
-    horz = cv2.normalize(flow[...,0], None, 0, 255, cv2.NORM_MINMAX)     
-    vert = cv2.normalize(flow[...,1], None, 0, 255, cv2.NORM_MINMAX)
-    horz = horz.astype('uint8')
-    vert = vert.astype('uint8')
+        flow = cv2.calcOpticalFlowFarneback(prvs,next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
 
-    cv2.imwrite(f"../frames/flow/flow_{num_frames}.jpg", rgb)
-    cv2.imwrite(f"../frames/vert/vert_{num_frames}.jpg", vert)
-    
-    prev_speed = speeds[num_frames]
-    this_speed = speeds[num_frames + 1]
-    
-    mean_speeds.append(np.mean([this_speed, prev_speed]))
-    num_frames += 1
-    if (num_frames+1) % 100 == 0:
-        print(f"Finished {num_frames} frames")
-    prvs = next
-    
-with open('mean_speeds', 'wb') as f:
-    pickle.dump(mean_speeds, f)
+        mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
+        hsv[...,0] = ang*180/np.pi/2
+        hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
+        rgb = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
+        
+        horz = cv2.normalize(flow[...,0], None, 0, 255, cv2.NORM_MINMAX)     
+        vert = cv2.normalize(flow[...,1], None, 0, 255, cv2.NORM_MINMAX)
+        horz = horz.astype('uint8')
+        vert = vert.astype('uint8')
 
-cap.release()
-cv2.destroyAllWindows()
+        cv2.imwrite(f"../frames/flow/flow_{num_frames}.jpg", rgb)
+        cv2.imwrite(f"../frames/vert/vert_{num_frames}.jpg", vert)
+        
+        num_frames += 1
+        if (num_frames+1) % 100 == 0:
+            print(f"Finished {num_frames} frames")
+        prvs = next
+        
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+def img_to_flow_test():
+    cap = cv2.VideoCapture("../video/test.mp4")
+    ret, frame1 = cap.read()
+    prvs = canny_edge_detector(frame1)
+    hsv = np.zeros_like(frame1)
+    hsv[...,1] = 255
+
+
+    num_frames = 0
+    while(1):
+        ret, frame2 = cap.read()
+        next = canny_edge_detector(frame2)
+
+        flow = cv2.calcOpticalFlowFarneback(prvs,next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+
+        mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
+        hsv[...,0] = ang*180/np.pi/2
+        hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
+        rgb = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
+        
+
+        cv2.imwrite(f"../frames/test_flow/flow_{num_frames}.jpg", rgb)
+        
+        num_frames += 1
+        if (num_frames+1) % 100 == 0:
+            print(f"Finished {num_frames} frames")
+        prvs = next
+        
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        img_to_flow_train()
+
+    else:
+        if sys.argv[1] == 'train':
+            img_to_flow_train()
+        elif sys.argv[1] == 'test':
+            img_to_flow_test()
+        else:
+            raise NotImplementedError("Argument must either be test or train")
--- data_scripts/transformation.py
+++ data_scripts/transformation.py
@@ -1,57 +1,110 @@
-import skimage.transform
-import cv2 as cv
 import numpy as np
+import re
+import cv2 as cv
+import tensorflow as tf
+import glob
+import pickle
+import matplotlib.pyplot as plt
+
+from random import shuffle
+
+
+from skimage.util import random_noise as noise
+from tqdm import tqdm
+
+def read(image_path):
+    image_string = tf.io.read_file(image_path)
+    image = tf.image.decode_jpeg(image_string,channels=3)
+    return image
 
 """
-Flips an image horizontally
+Applies Gaussian Noise to image
 """
-def flip(image):
-    return cv.flip(image, 1)
+def random_noise(image):
+    return noise(image.numpy()) 
+
 
 """
-Scales an image by a random factor
+Random Hue Augmentation
 """
-def scale(image):
-    scales = [.5, .75, 1.5, 2]
-
-    choice = np.random.choice(scales)
-
-    transformed = cv.resize(image, None, fx=choice, fy=choice, interpolation=cv.INTER_LINEAR if choice > 1 else cv.INTER_AREA)
+def random_hue(image):
+    return tf.image.random_hue(image,.05) 
 
 
-    return cv.resize(transformed, image.shape[:-1][::-1])
 """
-Randomly crops and resizes an image
+Applies the transformations and saves the images
 """
-def random_crop(image):
-    height, width, _ = image.shape
-    crop_width = int(width * .75)
-    crop_height = int(height * .75)
+def apply_transformations(image_path, speed):
+    orig = read(image_path)
+    augmented = [orig]
+    if speed > 25.5:
+        augmented.extend([random_noise(orig) for i in range(3)])
 
-    max_x = height - crop_height
-    max_y = width - crop_width
-
-    x = np.random.randint(0, max_x)
-    y = np.random.randint(0, max_y)
-
-    cropped = image[y: y+crop_height, x: x+crop_width]
-
-    resized = cv.resize(cropped, (width, height))
-
-    return resized
+    return augmented
 
 
+def natural_sort(l): 
+    convert = lambda text: int(text) if text.isdigit() else text.lower() 
+    alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ] 
+    return sorted(l, key = alphanum_key)
+
+
+def run():
+    images = natural_sort(glob.glob('../frames/flow/*.jpg'))
+    shuffle(images)
+    assert len(images) == 20399, f"Number of frames {len(images)}"
+    with open('../frames/mean_speeds', 'rb') as f:
+        speeds = pickle.load(f)
+
+    augmented_speeds = []
+    augmented_images = []
+
+    validation_images = []
+    validation_speeds = []
+    for i, img in enumerate(tqdm(images, desc="Augmenting")):
+        augmented = apply_transformations(img, speeds[i])
+        if (i+1) % 4 == 0:
+            validation_images.extend(augmented)
+            validation_speeds.extend([speeds[i]]*len(augmented))
+        else:
+            augmented_images.extend(augmented)
+            augmented_speeds.extend([speeds[i]]*len(augmented))
+
+    assert len(augmented_speeds) == len(augmented_images)
+    assert len(validation_speeds) == len(validation_images)
+
+    for i, img in enumerate(tqdm(augmented_images, desc="Saving")):
+        tf.keras.preprocessing.image.save_img(f'../frames/augmented/frame_{i}.jpg', img)
+
+    with open('../frames/augmented_speeds', 'wb') as f:
+        pickle.dump(augmented_speeds, f)
+
+    for i, img in enumerate(tqdm(validation_images, desc="Saving")):
+        tf.keras.preprocessing.image.save_img(f'../frames/validation/frame_{i}.jpg', img)
+
+    with open('../frames/validation_speeds', 'wb') as f:
+        pickle.dump(validation_speeds, f)
 
 if __name__ == '__main__':
-    original_image = 'frames/flow/flow_0.jpg'
-    image = cv.imread(original_image)
+    run()
+    #images = sorted(glob.glob('../frames/augmented/*.jpg'))
+    #with open('../frames/augmented_speeds', 'rb') as f:
+    #    speeds = pickle.load(f)
+    #assert len(images) == len(speeds)
+    #
+    #num_images = len(images)
+    #num_added = 1
+    #
+    #for i, image in enumerate(tqdm(images, desc="Balancing")):
+    #    if speeds[i] >= 26:
+    #        orig = read(image)
+    #        for j in range(2):
+    #            tf.keras.preprocessing.image.save_img(
+    #            f'../frames/augmented/frame_{num_images+num_added}.jpg',
+    #                        random_noise(orig))
+    #            num_added += 1
+    #            speeds.append(speeds[i])
 
-    cv.imshow('original', image)
-
-    transformed = random_crop(image)
-
-    cv.imshow('transformed', transformed)
-    while True:
-        if cv.waitKey(1) & 0xFF == ord('q'):
-             break
-    cv.destroyAllWindows()
+    #with open('../frames/augmented_speeds', 'wb') as f:
+    #    pickle.dump(speeds, f)
+    #assert len(speeds) == (len(images) + num_added - 1), f"Num speeds {len(speeds)}, Num Frames {len(images) + num_added - 1}"
