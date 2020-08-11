20b072e62a4a929b35eb0e69e7901b1ecd13080e
zanussbaum <zanussbaum@gmail.com>
2020-08-11 14:50:40 -0400

    uneven shapes

--- test_flow.py
+++ test_flow.py
@@ -32,10 +32,21 @@ def test_average_flow():
     assert np.all(avg == .04)
     assert avg.shape == (5, 5, 3)
 
+    flow2 = np.zeros((20, 40, 3))
+
+    for i in range(0, 20, 5):
+        for j in range(0, 40, 5):
+            flow2[i, j] = 1
+
+    avg = calc_avg_flow(flow2, row_stride=5, col_stride=5)
+
+    assert avg.shape == (4, 8, 3)
+    assert np.all(avg == .04)
+
 
 def test_odd_shapes():
     flow = np.zeros((25, 25, 3))
 
-    avg = calc_avg_flow(flow)
+    avg = calc_avg_flow(flow, row_stride=4, col_stride=4)
 
     assert avg.shape == (6, 6, 3)
