454441703f5b4729833efd02dba50f79d0e8c104
zanussbaum <zanussbaum@gmail.com>
2020-08-11 18:25:58 -0400

    loading model

--- model.py
+++ model.py
@@ -1,3 +1,5 @@
+import pickle
+
 import numpy as np
 
 from sklearn.linear_model import LinearRegression
@@ -5,7 +7,7 @@ from sklearn.linear_model import LinearRegression
 
 class Model:
     def __init__(self):
-        self.model = LinearRegression()
+        self.model = LinearRegression(normalize=True)
 
     def fit(self, x, y):
         self.model.fit(x, y)
@@ -20,3 +22,14 @@ class Model:
 
     def predict(self, x):
         return self.model.predict(x)
+
+    def save(self, save_path='model'):
+        with open(save_path, 'wb') as f:
+            pickle.dump(self, f)
+
+    @classmethod
+    def load(cls, model_path):
+        with open(model_path, 'rb') as f:
+            model = pickle.load(f)
+
+        return model
