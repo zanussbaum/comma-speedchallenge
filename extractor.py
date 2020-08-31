3f8e4b14d4c54dedcae427a344401b282bc7498c
zanussbaum <zanussbaum@gmail.com>
2020-08-30 23:33:01 -0700

    extracted pose from geo

--- extractor.py
+++ extractor.py
@@ -1,28 +1,49 @@
 import cv2
 import numpy as np
-
+np.set_printoptions(suppress=True)
 from skimage.measure import ransac
 from skimage.transform import FundamentalMatrixTransform
-
-FLANN_INDEX_KDTREE = 1
-FLANN_INDEX_LSH = 6
-flann_params = dict(algorithm=FLANN_INDEX_LSH,
-                    table_number=6,  # 12
-                    key_size=12,     # 20
-                    multi_probe_level=1)  # 2
 
 
 def make_homogenous(x):
     return np.concatenate([x, np.ones((x.shape[0], 1))], axis=1)
 
+def poseRt(R, t):
+    Rt = np.eye(4)
+    Rt[:3, :3] = R
+    Rt[:3, 3] = t
+    return Rt
+
+
+def extract_pose(E):
+    W = np.array([[0, -1, 0], [1, 0, 0], [0, 0, 1]])
+    U, d, Vt = np.linalg.svd(E)
+    if np.linalg.det(U) < 0:
+        U *= -1.0
+
+    if np.linalg.det(Vt) < 0:
+        Vt *= -1.0
+
+    R = np.dot(np.dot(U, W), Vt)
+    if np.sum(R.diagonal()) < 0:
+        R = np.dot(np.dot(U, W.T), Vt)
+
+    t = U[:, 2]
+    if t[2] < 0:
+        t *= 1
+
+    return np.linalg.inv(poseRt(R, t))
+
 
 class Extractor:
     def __init__(self, K):
-        self.extractor = cv2.ORB_create(nfeatures=50000)
-        self.matcher = cv2.FlannBasedMatcher(flann_params, {})
+        self.extractor = cv2.ORB_create()
+        self.matcher = cv2.BFMatcher(cv2.NORM_HAMMING)
         self.last = None
         self.K = K
         self.Kinv = np.linalg.inv(K)
+        self.Fx = K[0, 0]
+        self.Fy = K[1, 1]
 
     def normalize(self, pts):
         # K 3 x 3, pts is n x 3
@@ -31,16 +52,18 @@ class Extractor:
         return np.dot(self.Kinv, make_homogenous(pts).T).T[:, :2]
 
     def denormalize(self, pts):
-        denormed = np.dot(self.K, np.array([pts[0], pts[1], 1]).T)
-        return int(round(denormed[0])), int(round(denormed[1])) 
+        denormed = np.dot(self.K, np.array([pts[0], pts[1], 1.0]).T)
+        return int(round(denormed[0])), int(round(denormed[1]))
 
     def extract(self, img):
         # changing to gray scale
-        img = np.mean(img, axis=2).astype(np.uint8)
-        features = cv2.goodFeaturesToTrack(img, 3000, minDistance=3,
-                                           qualityLevel=.01)
-        kps, des = self.extractor.detectAndCompute(img, None,
-                                                   descriptors=features)
+        feats = cv2.goodFeaturesToTrack(np.mean(img, axis=2).astype(np.uint8),
+                                        3000, qualityLevel=0.01, minDistance=3)
+
+            # extraction
+        kps = [cv2.KeyPoint(x=f[0][0], y=f[0][1], _size=20) for f in feats]
+        kps, des = self.extractor.compute(img, kps)
+
         points = []
         if self.last is not None:
             matches = self.matcher.knnMatch(des, self.last['des'], k=2)
@@ -50,6 +73,7 @@ class Extractor:
                     kp1 = kps[m.queryIdx].pt
                     kp2 = self.last['kps'][m.trainIdx].pt
                     points.append((kp1, kp2))
+        Rt = None
         if len(points) > 0:
             points = np.array(points)
             points[:, 0, :] = self.normalize(points[:, 0, :])
@@ -57,10 +81,12 @@ class Extractor:
 
             model, inliers = ransac((points[:, 0], points[:, 1]),
                                     FundamentalMatrixTransform,
-                                    min_samples=8, residual_threshold=1,
+                                    min_samples=8, residual_threshold=0.01,
                                     max_trials=200)
             points = points[inliers]
-            print(model.params)
+
+            Rt = extract_pose(model.params)
+
 
         self.last = {'kps': kps, 'des': des}
-        return points
+        return points, Rt
--- vo.py
+++ vo.py
@@ -6,22 +6,22 @@ from extractor import Extractor
 W = 1920//2
 H = 1080//2
 
-F = 1
+F = 270
 K = np.array([[F, 0, W//2],[0, F, H//2],[0, 0, 1]])
 
 
 def process_frame(img, extractor):
-    matches = extractor.extract(img)
+    matches, Rt = extractor.extract(img)
     for pt1, pt2 in matches:
         u1, v1 = extractor.denormalize(pt1)
         u2, v2 = extractor.denormalize(pt2)
         cv2.circle(img, (u1, v1), 3, (0, 255, 0))
-        cv2.line(img, (u1, v1), (u2, v2), (0, 255, 0), 3)
+        cv2.line(img, (u1, v1), (u2, v2), (255, 0, 0))
 
     cv2.imshow('frame', img)
     cv2.waitKey(1)
 
-    return matches
+    return matches, Rt
 
 
 def capture(video):
@@ -33,10 +33,15 @@ def capture(video):
             break
 
         img = cv2.resize(frame, (W,H))
-        matches = process_frame(img, ext)
+        matches, pose = process_frame(img, ext)
+        print(f'Matches {len(matches)}')
+        print(pose)
 
+
+    cap.release()
     cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
-    capture('video/odo.mp4')
+    video = 'video/vo.mp4'
+    capture(video)
